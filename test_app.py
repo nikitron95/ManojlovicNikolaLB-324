0c331837a10398e68cb072350d009e3b3b5edf6e
N.Manojlovic.inf21 <n.manojlovic.inf21@stud.bbbaden.ch>
2023-09-28 19:49:50 +0200

    Updates

--- test_app.py
+++ test_app.py
@@ -16,15 +16,15 @@ def client():
 def test_add_entry_with_happiness(client):
     # Test adding an entry with happiness
     response = client.post(
-        '/add_entry', data={'content': 'Test Entry Content', 'happiness': '😃'})
+        "/add_entry", data={"content": "Test Entry Content", "happiness": "😃"}
+    )
 
     # Check if the response is a redirect to the index page
     assert response.status_code == 302
-    assert response.headers['Location'] == '/'
+    assert response.headers["Location"] == "/"
 
     # Check if the entry was added to the database with the correct happiness
     entry = entries[0]
     assert entry is not None
-    assert entry.content == 'Test Entry Content'
-    assert entry.happiness == '😃'
-
+    assert entry.content == "Test Entry Content"
+    assert entry.happiness == "😃"
